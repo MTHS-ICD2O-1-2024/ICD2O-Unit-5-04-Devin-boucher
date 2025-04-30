5f595ac075abfde08053e9dea8219800d740a629
default <devin.boucher@stu.ocsb.ca>
2025-04-30 20:29:05 +0000

    fixed format

--- js/script.js
+++ js/script.js
@@ -4,9 +4,7 @@
 // Created on: April 2025
 // This file contains the JS functions for index.html
 
-
 'use strict'
-
 
 // eslint-disable-next-line no-unused-vars
 function checkNumber () {
@@ -14,13 +12,11 @@ function checkNumber () {
   const day = document.getElementById('day').value
   let discount
 
-
   if (((age > 12) && (age < 21)) || (day === 'Tuesday') || (day === 'Thursday')) {
     discount = ('You get a discount at the museum!')
   } else {
     discount = ('You must pay full price at the museum.')
   }
-
 
   document.getElementById('result').innerHTML = `<p><strong>${discount}</strong></p>`
 }
