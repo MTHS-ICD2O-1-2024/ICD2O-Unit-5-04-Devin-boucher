eb248579f3dc0ad4396461625a494ff019025b28
default <devin.boucher@stu.ocsb.ca>
2025-04-30 20:24:45 +0000

    fixed code

--- js/script.js
+++ js/script.js
@@ -4,18 +4,23 @@
 // Created on: April 2025
 // This file contains the JS functions for index.html
 
+
 'use strict'
+
 
 // eslint-disable-next-line no-unused-vars
 function checkNumber () {
-  let age = parseFloat(document.getElementById('age').value)
+  const age = parseFloat(document.getElementById('age').value)
   const day = document.getElementById('day').value
+  let discount
+
 
   if (((age > 12) && (age < 21)) || (day === 'Tuesday') || (day === 'Thursday')) {
-    age = ('You get a discount at the museum!')
+    discount = ('You get a discount at the museum!')
   } else {
-    age = ('You must pay full price at the museum.')
+    discount = ('You must pay full price at the museum.')
   }
 
-  document.getElementById('result').innerHTML = `<p><strong>${age}</strong></p>`
+
+  document.getElementById('result').innerHTML = `<p><strong>${discount}</strong></p>`
 }
